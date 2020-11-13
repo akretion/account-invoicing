51ec6d08e3d80046f925a4afeacc6fee7fe212a9
Chafique <chafique.delli@akretion.com>
2020-11-13 12:52:05 +0100

    [FIX] tests

--- account_global_discount_amount/tests/test_account_global_discount_amount.py
+++ account_global_discount_amount/tests/test_account_global_discount_amount.py
@@ -7,97 +7,96 @@ class TestAccountGlobalDiscountAmount(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.partner = cls.env["res.partner"].create({"name": "Partner Test"})
-        cls.product01 = cls.env.ref("product.consu_delivery_01")
-        cls.product03 = cls.env.ref("product.consu_delivery_03")
+        partner = cls.env["res.partner"].create({"name": "Partner Test"})
+        product01 = cls.env.ref("product.consu_delivery_01")
+        product03 = cls.env.ref("product.consu_delivery_03")
         cls.discount_product = cls.env.ref(
             "account_global_discount_amount.discount_product")
-        cls.account = cls.env["account.account"].search(
-            [("user_type_id", "=",
-              cls.env.ref("account.data_account_type_revenue").id)], limit=1)
-        cls.journal = cls.env["account.journal"].search(
-            [("type", "=", "sale")], limit=1)
-        cls.type_current_liability = cls.env.ref(
+        account = cls.env["account.account"].search([
+            ("user_type_id", "=",
+             cls.env.ref("account.data_account_type_revenue").id)], limit=1)
+        journal = cls.env["account.journal"].search([
+            ("type", "=", "sale")], limit=1)
+        type_current_liability = cls.env.ref(
             "account.data_account_type_current_liabilities")
-        cls.output_vat10_acct = cls.env["account.account"].create(
+        output_vat10_acct = cls.env["account.account"].create(
             {"name": "10",
              "code": "10",
              "user_type_id": type_current_liability.id})
-        cls.output_vat20_acct = cls.env["account.account"].create(
+        output_vat20_acct = cls.env["account.account"].create(
             {"name": "20",
              "code": "20",
              "user_type_id": type_current_liability.id})
-        cls.tax_group_vat10 = cls.env["account.tax.group"].create({
+        tax_group_vat10 = cls.env["account.tax.group"].create({
             "name": "VAT10"})
-        cls.tax_group_vat20 = cls.env["account.tax.group"].create({
+        tax_group_vat20 = cls.env["account.tax.group"].create({
             "name": "VAT20"})
-        cls.vat10 = cls.env["account.tax"].create(
-            {"name": "TEST 10%",
-             "type_tax_use": "sale",
-             "amount_type": "percent",
-             "amount": 10.00,
-             "tax_group_id": cls.tax_group_vat10.id,
-             "invoice_repartition_line_ids": [
+        cls.vat10 = cls.env["account.tax"].create({
+            "name": "TEST 10%",
+            "type_tax_use": "sale",
+            "amount_type": "percent",
+            "amount": 10.00,
+            "tax_group_id": tax_group_vat10.id,
+            "invoice_repartition_line_ids": [
                 (0, 0, {"factor_percent": 100.0, "repartition_type": "base"}),
                 (0, 0, {"factor_percent": 100.0, "repartition_type": "tax",
-                        "account_id": cls.output_vat10_acct.id})],
-            }
-        cls.vat20 = cls.env["account.tax"].create(
-            {"name": "TEST 20%",
-             "type_tax_use": "sale",
-             "amount_type": "percent",
-             "amount": 20.00,
-             "tax_group_id": cls.tax_group_vat20.id,
-             "invoice_repartition_line_ids": [
+                        "account_id": output_vat10_acct.id})],
+        })
+        cls.vat20 = cls.env["account.tax"].create({
+            "name": "TEST 20%",
+            "type_tax_use": "sale",
+            "amount_type": "percent",
+            "amount": 20.00,
+            "tax_group_id": tax_group_vat20.id,
+            "invoice_repartition_line_ids": [
                 (0, 0, {"factor_percent": 100.0, "repartition_type": "base"}),
                 (0, 0, {"factor_percent": 100.0, "repartition_type": "tax",
-                        "account_id": cls.output_vat20_acct.id})],
-            }
+                        "account_id": output_vat20_acct.id})],
+        })
         single_tax_invoice_line_vals = [
             (0, 0, {
-                    "product_id": self.product01.id,
-                    "quantity": 1.0,
-                    "account_id": self.account.id,
-                    "name": "Line 1",
-                    "price_unit": 200.00,
-                    "tax_ids": [(6, 0, [self.vat10.id])],
-                },
-            )
+                "product_id": product01.id,
+                "quantity": 1.0,
+                "account_id": account.id,
+                "name": "Line 1",
+                "price_unit": 200.00,
+                "tax_ids": [(6, 0, [cls.vat10.id])],
+            })
         ]
-        cls.invoice_single_tax = cls.env["account.move"].create({
-                    "journal_id": self.journal.id,
-                    "partner_id": self.partner.id,
-                    "type": "out_invoice",
-                    "invoice_line_ids": single_tax_invoice_line_vals,
-                    "global_discount_amount": 30.00,
-                })
+        cls.invoice_single_tax = cls.env["account.move"].with_context(
+            {"check_move_validity": False}).create({
+                "journal_id": journal.id,
+                "partner_id": partner.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": single_tax_invoice_line_vals,
+                "global_discount_amount": 30.00,
+            })
         multi_tax_invoice_line_vals = [
             (0, 0, {
-                    "product_id": self.product01.id,
-                    "quantity": 1.0,
-                    "account_id": self.account.id,
-                    "name": "Line 1",
-                    "price_unit": 200.00,
-                    "tax_ids": [(6, 0, [self.vat10.id])],
-                },
-            ),
+                "product_id": product01.id,
+                "quantity": 1.0,
+                "account_id": account.id,
+                "name": "Line 1",
+                "price_unit": 200.00,
+                "tax_ids": [(6, 0, [cls.vat10.id])],
+            }),
             (0, 0, {
-                    "product_id": self.product03.id,
-                    "quantity": 1.0,
-                    "account_id": self.account.id,
-                    "name": "Line 1",
-                    "price_unit": 150.00,
-                    "tax_ids": [(6, 0, [self.vat20.id])],
-                },
-            )
+                "product_id": product03.id,
+                "quantity": 1.0,
+                "account_id": account.id,
+                "name": "Line 1",
+                "price_unit": 150.00,
+                "tax_ids": [(6, 0, [cls.vat20.id])],
+            })
         ]
-        cls.invoice_multi_tax = cls.env["account.move"].create({
-                    "journal_id": self.journal.id,
-                    "partner_id": self.partner.id,
-                    "type": "out_invoice",
-                    "invoice_line_ids": multi_tax_invoice_line_vals,
-                    "global_discount_amount": 50.00,
-                })
+        cls.invoice_multi_tax = cls.env["account.move"].with_context(
+            {"check_move_validity": False}).create({
+                "journal_id": journal.id,
+                "partner_id": partner.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": multi_tax_invoice_line_vals,
+                "global_discount_amount": 50.00,
+            })
 
     def test_create_invoice_single_tax_with_global_discount(self):
         self.assertEqual(self.invoice_single_tax.amount_total, 187.00)
@@ -116,8 +115,8 @@ class TestAccountGlobalDiscountAmount(SavepointCase):
 
     def test_create_invoice_multi_tax_with_global_discount(self):
         self.assertEqual(self.invoice_multi_tax.amount_untaxed, 300.00)
-        self.assertEqual(self.invoice_multi_tax.amount_tax, 42.86)
-        self.assertEqual(self.invoice_multi_tax.amount_total, 342.86)
+        self.assertEqual(self.invoice_multi_tax.amount_tax, 42.85)
+        self.assertEqual(self.invoice_multi_tax.amount_total, 342.85)
         self.assertEqual(self.invoice_multi_tax.global_discount_ok, True)
         discount_move_lines = self.env["account.move.line"].search([
             ("product_id", "=", self.discount_product.id),
@@ -164,7 +163,7 @@ class TestAccountGlobalDiscountAmount(SavepointCase):
 
     def test_write_invoice_single_tax_with_null_global_discount(self):
         self.invoice_single_tax.write({"global_discount_amount": 0.00})
-        self.assertEqual(self.invoice_single_tax.amount_total, 200.00)
+        self.assertEqual(self.invoice_single_tax.amount_total, 220.00)
         self.assertEqual(self.invoice_single_tax.amount_tax, 20.00)
         self.assertEqual(self.invoice_single_tax.global_discount_ok, True)
         discount_move_lines = self.env["account.move.line"].search([
